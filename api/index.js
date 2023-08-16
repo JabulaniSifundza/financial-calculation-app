d7efa9bd219671295a3d1ed9c168fb6e3f417c4b
JabulaniSifundza <jjsifundza@gmail.com>
2023-08-16 11:46:20 +0200

    Server test

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/home', (req, res) => {
+    //res.send('Hello, World!');
+    res.sendFile('/public/index.html')
 });
 
 app.post('/api/capm', (req, res) => {
     res.send('Hello, World!')
+});
+
+app.get('/my-html-file', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(3000, () => {
