f88d8436ded3382848a0f92b90b69120988e561f
JabulaniSifundza <jjsifundza@gmail.com>
2023-08-16 12:00:49 +0200

    Routing update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/home', (req, res) => {
     //res.send('Hello, World!');
     res.sendFile('/public/index.html')
