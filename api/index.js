8da379780d8eb6e8bee0ec7345e4169a79ef5eda
JabulaniSifundza <jjsifundza@gmail.com>
2023-09-21 20:24:18 +0200

    Vercel Hosting

---
+++ api/index.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const yahooFinance = require('yahoo-finance2').default;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//parse JSON data
+app.use(express.json())
+app.use(express.static('public'));
+
+const stock_ticker_data = {}
+const company_financial_data = {}
+
+app.get('/home', (req, res) => {
+    //res.send('Hello, World!');
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/api/capm', async(req, res) => {
+    const {ticker_symbols, benchmark_ticker} = req.body;
+    try{
+        const company_result = {}
+        const benchmark_data = {}
+        const beta_result = {}
+        const query_options = { period1: '2005-01-01'}
+        const risk_free_query_options = { period1: '2023-08-14', period2: '2023-08-15'}
+        const beta_data_query_options = { modules: ['summaryDetail', 'balanceSheetHistory', 'cashflowStatementHistory',  'incomeStatementHistory', 'financialData'] };
+
+        const risk_free_data = await yahooFinance.historical("^TNX", risk_free_query_options)
+        const benchmark_result = await yahooFinance.historical(benchmark_ticker, query_options)
+
+        benchmark_data[benchmark_ticker] = benchmark_result
+        for(const symbol of ticker_symbols){
+            //company_result.push(await yahooFinance.historical(symbol, query_options))
+            const ticker_data = await yahooFinance.historical(symbol, query_options)
+            stock_ticker_data[symbol] = ticker_data
+            company_result[symbol] = ticker_data
+        }
+
+        for(const ticker of ticker_symbols){
+            const financial_data = await yahooFinance.quoteSummary(ticker, beta_data_query_options)
+            beta_result[ticker] = financial_data.summaryDetail.beta
+            company_financial_data[ticker] = [financial_data.balanceSheetHistory, financial_data.cashflowStatementHistory, financial_data.incomeStatementHistory, financial_data.financialData]
+        }
+        res.status(200).json({benchmark_data, company_result, risk_free_data, beta_result})
+    }
+    catch(error){
+        res.status(500).json({error: error.name, msg: error.message})
+    }
+});
+
+app.post('/api/simple', async(req, res)=>{
+    const {ticker_symbol} = req.body
+    try{
+        const data = stock_ticker_data[ticker_symbol]
+        res.status(200).json({data})
+    }
+    catch(error){
+        res.status(500).json({error: error.name, msg: error.message})
+    }
+})
+
+app.post('/api/company-financials', async(req, res)=>{
+    const {ticker_symbol} = req.body
+    try{
+        const data = company_financial_data[ticker_symbol]
+        res.status(200).json({data})
+    }
+    catch(error){
+        res.status(500).json({error: error.name, msg: error.message})
+    }
+})
+
+app.post('/api/portfolio-data', async(req, res)=>{
+    const {portfolio_name} = req.body
+    try{
+        const data = stock_ticker_data
+        res.status(200).json({data})
+    }
+    catch(error){
+        res.status(500).json({error: error.name, msg: error.message})
+    }
+})
+
+app.post('/api/monte-carlo', async(req, res)=>{
+    const {ticker_symbol} = req.body
+    try{
+        const data = stock_ticker_data[ticker_symbol]
+        res.status(200).json({data})
+    }
+    catch(error){
+        res.status(500).json({error: error.name, msg: error.message})
+    }
+})
+
+app.post('/api/value-at-risk', async(req, res)=>{
+    const {ticker_symbol} = req.body
+    try{
+        const data = stock_ticker_data
+        res.status(200).json({data})
+    }
+    catch(error){
+        res.status(500).json({error: error.name, msg: error.message})
+    }
+})
+
+app.post('/api/save-company-financial-data', async(req, res)=>{
+    const {ticker_symbol} = req.body
+    try{
+        const data = stock_ticker_data[ticker_symbol]
+        const financial_data = company_financial_data[ticker_symbol]
+        res.status(200).json({stock_price_data: data, financial_data: financial_data})
+    }
+    catch(error){
+        res.status(500).json({error: error.name, msg: error.message})
+    }
+})
+
+app.get('/my-html-file', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
